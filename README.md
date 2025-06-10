cc1427230e3f40c76dc0229425e146f203a1b47f
unknown <info@sangusit.com>
2025-06-09 22:43:59 -0400

    extracted sample QBL syntax

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-Sample app for test MERN app generation from QBL syntax
+Sample app to test MERN app generation from QBL syntax
 
 Follow instructions at this [link](https://www.mongodb.com/resources/languages/mern-stack-tutorial) to set up base app from scratch, otherwise use this repo as a temple:
 
---
+++ main.py
@@ -0,0 +1,12 @@
+def get_config():
+    try:
+        with open('sample_qbl.yaml', 'r', encoding="utf8", errors="ignore") as file:
+            data = file.read()
+        return data
+    except Exception as exc:
+        print(exc)
+
+config = get_config()
+config_split = config.split('\n')
+print(config_split[0].split(':'))
+# if second element is blank then it's a header
